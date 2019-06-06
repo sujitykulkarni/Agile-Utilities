4c4f7159cf674408d24a31bc7e925f6cb491be9e
Sujit <sujit.kulkarni@rapidcircle.com>
2019-06-06 15:28:37 +0530

    Updates

---
+++ precache-manifest.0063d7f72c75cb03200bc314815d67e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bffcb1c99e19792cb49e8bc6397cdc",
+    "url": "/jira-time-calculator/index.html"
+  },
+  {
+    "revision": "79eb2a71d97f340c9512",
+    "url": "/jira-time-calculator/static/css/main.d491fab3.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/jira-time-calculator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "79eb2a71d97f340c9512",
+    "url": "/jira-time-calculator/static/js/main.53a1ccac.chunk.js"
+  },
+  {
+    "revision": "9225ad94d82e7cbb103a",
+    "url": "/jira-time-calculator/static/js/runtime~main.d0bbcadf.js"
+  }
+]);
